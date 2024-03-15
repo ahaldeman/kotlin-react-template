22bf2e9a17a1500b4e9935d1a90fe5abcebf0f1a
Alex Haldeman <alexhldmn5@gmail.com>
2024-03-14 23:58:06 -0400

    Initial project template

---
+++ src/main/kotlin/com/ahaldeman/kotlinreacttemplate/KotlinReactTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.ahaldeman.kotlinreacttemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinReactTemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinReactTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/ahaldeman/kotlinreacttemplate/KotlinReactTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ahaldeman.kotlinreacttemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinReactTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
